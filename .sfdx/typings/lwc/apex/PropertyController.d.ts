a45dfbf1d9dc0b70f8392bad24753fbba970951b
jtreeves <jackson.t.reeves@gmail.com>
2021-10-02 16:27:14 -0400

    Add initial files from local

---
+++ .sfdx/typings/lwc/apex/PropertyController.d.ts
@@ -0,0 +1,6 @@
+declare module "@salesforce/apex/PropertyController.getPagedPropertyList" {
+  export default function getPagedPropertyList(param: {searchKey: any, maxPrice: any, minBedrooms: any, minBathrooms: any, pageSize: any, pageNumber: any}): Promise<any>;
+}
+declare module "@salesforce/apex/PropertyController.getPictures" {
+  export default function getPictures(param: {propertyId: any}): Promise<any>;
+}
---
+++ .sfdx/typings/lwc/apex/SampleDataController.d.ts
@@ -0,0 +1,3 @@
+declare module "@salesforce/apex/SampleDataController.importSampleData" {
+  export default function importSampleData(): Promise<any>;
+}
